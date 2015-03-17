8d40e1c843e727525eae6ff475066f3ab64a6f10
zhdi <a571710306@163.com>
2015-03-17 20:30:11 +0800

    first improved

--- src/HellowWorld.java
+++ src/HellowWorld.java
@@ -4,6 +4,7 @@ public class HellowWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hellow world!");
+		System.out.println("the improved");
 	}
 
 }
